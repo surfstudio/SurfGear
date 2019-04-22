cef88e45e40ca51e2a7da67685681d5a060ce65a
Alexey Korpatenkov <akorpatenkov@surfstudio.co>
2019-04-22 18:50:47 +0300

    KDB-423 мекие правки в адаптере

--- network/lib/src/rx/rx_call_adapter.dart
+++ network/lib/src/rx/rx_call_adapter.dart
@@ -5,6 +5,6 @@ import 'package:rxdart/rxdart.dart';
 class RxCallAdapter<T> implements CallAdapter<Future<T>, Observable<T>> {
   @override
   Observable<T> adapt(Future<T> call) {
-    return Stream.fromFuture(call);
+    return Observable.fromFuture(call);
   }
 }
