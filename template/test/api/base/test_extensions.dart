c4178c47077daae8fe6b339e5bf1d08b3c90ede3
unknown <andrianov44@gmail.com>
2019-05-16 10:43:08 +0200

    ANDDEP-485-test_extensions_user_test

---
+++ template/test/api/base/test_extensions.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rxdart/rxdart.dart';
+
+/// Утилиты для тестирования потоков данных
+
+/// Проверка полученных из [Observable] данных на правду
+void expectTrue(Observable<bool> request) => expect(request, emits(true));
+
+/// Проверка полученных из [Observable] данных на null
+void expectNotNull(Observable request) => expectTrue(request.map((r) => r != null));
+
+/// Проверка полученного из [Observable] списка на пустоту
+void expectNotEmpty(Observable<List> request) => expectTrue(request.map((r) => r.isNotEmpty));
