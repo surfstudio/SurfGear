f2717b1161e876db9df79a82460e271dcce1528d
zaytsev <zaytsev@srfstudio.ru>
2019-05-16 12:50:08 +0300

    add some docs about Fabric and build scripts. Minor refactor

--- template/lib/runner/runner.dart
+++ template/lib/runner/runner.dart
@@ -1,40 +1,44 @@
 import 'dart:async';
 
-import 'package:flutter_crashlytics/flutter_crashlytics.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_crashlytics/flutter_crashlytics.dart';
 import 'package:flutter_template/config/base/env/env.dart';
 import 'package:flutter_template/ui/app/app.dart';
 import 'package:logger/logger.dart';
 
 void run() async {
-  // закрепляем ориентацию
+  // закрепляем ориентацию todo изменить на необходимое или убрать
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  await _initCrashlytics();
+  _initCrashlytics();
   _initLogger();
   _runApp();
 }
 
 void _runApp() {
-  runZoned<Future<Null>>(() async {
-    runApp(App());
-  }, onError:(error, stack) async {
-    await FlutterCrashlytics().reportCrash(error, stack, forceCrash: true);
-  },
+  runZoned<Future<Null>>(
+    () async {
+      runApp(App());
+    },
+    onError: (error, stack) async {
+      await FlutterCrashlytics().reportCrash(
+        error,
+        stack,
+        forceCrash: true, //todo убрать forceCrash
+      );
+    },
   );
 }
 
 void _initCrashlytics() async {
   bool isDebug = Environment.instance().isDebug;
-  //Crashlytics.instance.enableInDevMode = isDebug;
-  FlutterError.onError = (FlutterErrorDetails details)  async {
+  FlutterError.onError = (FlutterErrorDetails details) async {
     if (isDebug) {
       // In development mode simply print to console.
       FlutterError.dumpErrorToConsole(details);
     } else {
       // In production mode report to the application zone to report to
-      // Crashlytics.
       Zone.current.handleUncaughtError(details.exception, details.stack);
     }
   };
