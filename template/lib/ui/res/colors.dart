b80fa0dfb422f1f8b72839869f325b91f0aceb03
unknown <andrianov44@gmail.com>
2019-05-16 10:13:55 +0200

    ANDDEP-484-cons-colors-опечатка

--- template/lib/ui/res/colors.dart
+++ template/lib/ui/res/colors.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 // Семантические алиасы
 // Пополняютсфя по мере необходимости
 // Используют только основную палитру
-Color colorAccent = _purple,
+const Color colorAccent = _purple,
     colorError = _freeSpeechRed,
     colorRed = _alizarin,
     btnColor = _alizarin,
@@ -28,20 +28,20 @@ Color colorAccent = _purple,
     textColorGreen = _green;
 
 // Основная палитра (http://www.color-blindness.com/color-name-hue/)
-Color _green = Color(0xFF48AD41),
-    _sapphire = Color(0xFF0A3567),
-    _oxxfordBlue = Color(0xFF263238),
-    _purple = Color(0xFF7A3E93),
-    _freeSpeechRed = Color(0xFFB00000),
-    _alizarin = Color(0xFFE31E24),
-    _alizarin_10 = Color(0xFFF8E4E5),
-    _cupid = Color(0xFFF5BEC2),
-    _lightPink = Color(0xFFF9E8E6),
+const Color _green = const Color(0xFF48AD41),
+    _sapphire = const Color(0xFF0A3567),
+    _oxxfordBlue = const Color(0xFF263238),
+    _purple = const Color(0xFF7A3E93),
+    _freeSpeechRed = const Color(0xFFB00000),
+    _alizarin = const Color(0xFFE31E24),
+    _alizarin_10 = const Color(0xFFF8E4E5),
+    _cupid = const Color(0xFFF5BEC2),
+    _lightPink = const Color(0xFFF9E8E6),
     _white = Colors.white,
-    _pastelWhite = Color(0xFFFAFAFA),
-    _solitude = Color(0xFFE5E8EB),
-    _silver = Color(0xFFBDBDBD),
-    _darkGrey = Color(0xFFA7A7A7),
-    _whiteSmoke = Color(0xFFEEEEEE),
-    _grey = Color(0x8A000000),
-    _empress = Color(0xFF757575);
+    _pastelWhite = const Color(0xFFFAFAFA),
+    _solitude = const Color(0xFFE5E8EB),
+    _silver = const Color(0xFFBDBDBD),
+    _darkGrey = const Color(0xFFA7A7A7),
+    _whiteSmoke = const Color(0xFFEEEEEE),
+    _grey = const Color(0x8A000000),
+    _empress = const Color(0xFF757575);
