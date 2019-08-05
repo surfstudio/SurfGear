58ac11eabdbbf58d6da695240f13e4c0f012bda0
Aleksey Radionov <radionov@surfstudio.co>
2019-08-05 15:51:11 +0300

    ANDDEP-628 удаление зависимости от mvvm и injector

--- logger/example/lib/ui/screen/counter/counter_route.dart
+++
@@ -1,7 +0,0 @@
-import 'package:counter/ui/screen/counter/counter_screen.dart';
-import 'package:flutter/material.dart';
-
-/// Route для экрана счетчика
-class CounterScreenRoute extends MaterialPageRoute {
-  CounterScreenRoute() : super(builder: (ctx) => CounterScreen());
-}
