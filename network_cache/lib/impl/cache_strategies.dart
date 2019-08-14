a3723cb83f45a9b74fec44a87204baeb648cf562
Anton Dokusov <dokoussov@gmail.com>
2019-08-14 17:06:25 +0300

    created network cache module

---
+++ network_cache/lib/impl/cache_strategies.dart
@@ -0,0 +1,26 @@
+import 'package:network/network.dart';
+import 'package:network_cache/base/cache_strategy.dart';
+import 'package:rxdart/rxdart.dart';
+
+/// Emits cache response first, network response later.
+class CacheFirstStrategy implements CacheStrategy {
+  @override
+  Observable<Response> resolve(
+    Observable<Response> cacheResponse,
+    Observable<Response> networkResponse,
+  ) =>
+      Observable.concatEager([
+        cacheResponse,
+        networkResponse,
+      ]);
+}
+
+/// Emits cache response, if it doesn't exist emits network response.
+class CacheIfExistsStrategy implements CacheStrategy {
+  @override
+  Observable<Response> resolve(
+    Observable<Response> cacheResponse,
+    Observable<Response> networkResponse,
+  ) =>
+      cacheResponse.switchIfEmpty(networkResponse);
+}
