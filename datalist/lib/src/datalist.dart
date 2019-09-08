6d091079e33e9068c17353e5718e58e82f71c6a5
vladbelenkov <Aa123456>
2019-09-08 14:36:44 +0500

    ANDDEP-543-datalist-belenkov 1) Add DataList to separate packackage 2) Page-count mechanism not exist 3) Add doc from android-standart 4) Edit some incorrect test's

---
+++ datalist/lib/src/datalist.dart
@@ -0,0 +1,218 @@
+import 'dart:collection';
+import 'dart:core';
+
+import 'package:collection/collection.dart';
+import 'package:datalist/src/exceptions.dart';
+
+/// List для работы с пагинацией
+/// Механизм limit-offset
+/// Можно сливать с другим DataList
+///
+/// @param <T> Item
+class DataList<T> extends DelegatingList<T> {
+  //количество элементов в списке
+  int limit;
+
+  //сдвиг относительно первого элемента
+  int offset;
+
+  //максимально возможное количество эелементов списка
+  int totalCount;
+
+  List<T> data;
+
+  factory DataList.empty() {
+    return new DataList<T>(data: List<T>());
+  }
+
+  DataList<T> emptyWithTotal(int totalCount) {
+    return new DataList(data: List<T>(), totalCount: totalCount);
+  }
+
+  DataList({
+    this.data,
+    this.limit = 0,
+    this.offset = 0,
+    this.totalCount = 0,
+  }) : super(data);
+
+  /// Слияние двух DataList
+  ///
+  /// @param data DataList для слияния с текущим
+  /// @return текущий экземпляр
+  DataList<T> merge(DataList<T> data) {
+    bool reverse = data.offset < this.offset;
+    List<T> merged = tryMerge(reverse ? data : this, reverse ? this : data);
+    if (merged == null) {
+      //Отрезки данных не совпадают, слияние не возможно
+      throw IncompatibleRangesException("incorrect data range");
+    }
+    this.data.clear();
+    this.data.addAll(merged);
+    if (this.offset < data.offset) {
+      //загрузка вниз, как обычно
+      this.limit = data.offset + data.limit - this.offset;
+    } else if (this.offset == data.offset) {
+      //коллизия?
+      this.limit = data.limit;
+    } else {
+      // загрузка вверх
+      this.offset = data.offset;
+      this.limit = length;
+    }
+
+    this.totalCount = data.totalCount;
+    return this;
+  }
+
+  /// Слияние двух DataList с удалением дублируемых элементов
+  /// При удалении остаются актуальные (последние присланные сервером) элементы
+  ///
+  /// @param data              DataList для слияния с текущим
+  /// @param distinctPredicate предикат, по которому происходит удаление дублируемых элементов
+  /// @return текущий экземпляр
+  DataList<T> mergeWithPredicate<R>(
+    DataList<T> data,
+    R Function(T item) distinctPredicate,
+  ) {
+    bool reverse = data.offset < this.offset;
+    List<T> merged = tryMerge(reverse ? data : this, reverse ? this : data);
+    if (merged == null) {
+      //Отрезки данных не совпадают, слияние не возможно
+      throw new IncompatibleRangesException("incorrect data range");
+    }
+
+    List<T> filtered = distinctByLast(merged, distinctPredicate);
+    this.data.clear();
+    this.data.addAll(filtered);
+    if (this.offset < data.offset) {
+      //загрузка вниз, как обычно
+      this.limit = data.offset + data.limit - this.offset;
+    } else if (this.offset == data.offset) {
+      //коллизия?
+      this.limit = data.limit;
+    } else {
+      // загрузка вверх
+      this.offset = data.offset;
+      this.limit = length;
+    }
+
+    this.totalCount = data.totalCount;
+    return this;
+  }
+
+  /// Преобразует dataList одного типа в dataList другого типа
+  ///
+  /// @param mapFunc функция преобразования
+  /// @param <R>     тип данных нового списка
+  /// @return DataList с элементами типа R
+  DataList<R> transform<R>(R Function(T item) mapFunc) {
+    List<R> resultData = new List();
+    for (T item in this) {
+      resultData.add(mapFunc.call(item));
+    }
+
+    return DataList<R>(
+      data: resultData,
+      limit: limit,
+      offset: offset,
+      totalCount: totalCount,
+    );
+  }
+
+  /// возвращает значение offset c которого нужно начать чтобы подгрузить слкдующий блок данных
+  int get nextOffset => limit + offset;
+
+  int getLimit() {
+    return limit;
+  }
+
+  int getOffset() {
+    return offset;
+  }
+
+  int getTotalCount() {
+    return totalCount;
+  }
+
+  /// Проверка возможности дозагрузки данных
+  ///
+  /// @return
+  bool canGetMore() => totalCount > limit + offset;
+
+  List<T> tryMerge(DataList<T> to, DataList<T> from) {
+    if ((to.offset + to.limit) >= from.offset) {
+      return mergeLists(to.data, from.data, from.offset - to.offset);
+    }
+
+    return null;
+  }
+
+  List<T> mergeLists(List<T> to, List<T> from, int start) {
+    List<T> result = new List();
+    result.addAll(start < to.length ? to.sublist(0, start) : to);
+    result.addAll(from);
+    return result;
+  }
+
+  @override
+  bool add(T t) {
+    throw Exception("Unsupported operation \'add\'");
+  }
+
+  @override
+  bool remove(Object o) {
+    throw Exception("Unsupported operation \'remove\'");
+  }
+
+  bool containsAll(Iterable<dynamic> another) {
+    bool contains = false;
+    for (var c in another) {
+      contains = data.contains(c);
+    }
+
+    return contains;
+  }
+
+  @override
+  void addAll(Iterable<T> iterable) {
+    throw Exception("Unsupported operation \'addAll\'");
+  }
+
+  @override
+  void clear() {
+    super.clear();
+    limit = 0;
+    offset = 0;
+  }
+
+  @override
+  List<T> sublist(int start, [int end]) {
+    throw new Exception("Unsupported operation \'sublist\'");
+  }
+
+  @override
+  String toString() {
+    return "DataList {limit= $limit , offset= $offset , data= $data }";
+  }
+
+  /// Удаление одинаковых элементов из исходного списка
+  /// Критерий того, что элементы одинаковые, задается параметром distinctPredicate
+  ///
+  /// @param source            исходный список
+  /// @param distinctPredicate критерий того, что элементы одинаковые
+  /// @return отфильтрованный список без одинаковых элементов
+  List<T> distinctByLast<R>(
+    List<T> source,
+    R Function(T item) distinctPredicate,
+  ) {
+    LinkedHashMap<R, T> resultSet = LinkedHashMap<R, T>.of({});
+
+    for (T element in source) {
+      R key = distinctPredicate.call(element);
+      resultSet[key] = element;
+    }
+
+    return resultSet.values.toList();
+  }
+}
---
+++ datalist/test/datalist_test.dart
@@ -0,0 +1,372 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:datalist/datalist.dart';
+
+void main() {
+  DataListTest.testCheckNormalMerge();
+  DataListTest.checkNormalMergeWithOffset();
+  DataListTest.checkNormalMergeWithOffsetAndCollision();
+  DataListTest.checkMergeWithCollision();
+  DataListTest.checkMergeWithCollision2();
+  DataListTest.checkMergeWithCollision3();
+  DataListTest.checkMergeWithCollision4();
+  DataListTest.checkMergeEmptyWithNormal();
+  DataListTest.checkInvalidData();
+  DataListTest.transformToStringDataList();
+  DataListTest.checkDynamicDataInsertion();
+  DataListTest.checkDynamicDataInsertion2();
+  DataListTest.checkDynamicDataInsertion3();
+}
+
+class DataListTest {
+  static void testCheckNormalMerge() {
+    test(
+      "testCheckNormalMerge",
+      () {
+        DataList<int> list1 = DataList<int>(
+          data: [1, 2, 3, 4, 5],
+          limit: 5,
+          offset: 0,
+          totalCount: 10,
+        );
+        DataList<int> list2 = DataList(
+          data: [6, 7, 8, 9, 10],
+          limit: 5,
+          offset: 5,
+          totalCount: 5,
+        );
+        DataList<int> list3 = DataList(
+          data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+          limit: 10,
+          offset: 0,
+          totalCount: 10,
+        );
+        list1.merge(list2);
+        expect(list3, list1);
+      },
+    );
+  }
+
+  static void checkNormalMergeWithOffset() {
+    test("checkNormalMergeWithOffset", () {
+      DataList<int> list1 = DataList<int>(
+        data: [1, 2, 3, 4, 5],
+        limit: 5,
+        offset: 7,
+        totalCount: 10,
+      );
+      DataList<int> list2 = DataList(
+        data: [6, 7, 8, 9, 10],
+        limit: 5,
+        offset: 12,
+        totalCount: 10,
+      );
+      DataList<int> list3 = DataList(
+        data: [6, 7, 8, 9, 10],
+        limit: 5,
+        offset: 17,
+        totalCount: 10,
+      );
+      DataList<int> list4 = DataList(
+          data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 6, 7, 8, 9, 10],
+          limit: 15,
+          offset: 7,
+          totalCount: 10);
+      list3.merge(list2);
+      list3.merge(list1);
+      expect(list3, list4);
+    });
+  }
+
+  static void checkNormalMergeWithOffsetAndCollision() {
+    test('checkNormalMergeWithOffsetAndCollision', () {
+      DataList<int> list1 = DataList<int>(
+          data: [1, 2, 3, 4, 5], limit: 5, offset: 7, totalCount: 10);
+      DataList<int> list2 = DataList(
+          data: [6, 7, 8, 9, 10], limit: 5, offset: 12, totalCount: 10);
+      DataList<int> list3 = DataList(
+          data: [6, 7, 8, 9, 10], limit: 5, offset: 14, totalCount: 10);
+      DataList<int> list4 = DataList(
+          data: [1, 2, 3, 4, 5, 6, 7, 6, 7, 8, 9, 10],
+          limit: 12,
+          offset: 7,
+          totalCount: 10);
+      list3.merge(list2);
+      list3.merge(list1);
+      expect(list3, list4);
+    });
+  }
+
+  static void checkMergeWithCollision() {
+    test('checkMergeWithCollision', () {
+      DataList<int> list1 =
+          DataList(data: [1, 2, 3], limit: 3, offset: 0, totalCount: 10);
+      DataList<int> list2 =
+          DataList(data: [6, 7], limit: 3, offset: 0, totalCount: 10);
+      DataList<int> list3 =
+          DataList(data: [6, 7], limit: 3, offset: 0, totalCount: 10);
+      list1.merge(list2);
+      expect(list1, list3);
+    });
+  }
+
+  static void checkMergeWithCollision2() {
+    test('checkMergeWithCollision2', () {
+      DataList<int> list1 =
+          DataList(data: [1, 2, 3], limit: 3, offset: 2, totalCount: 10);
+      DataList<int> list2 =
+          DataList(data: [4, 5], limit: 3, offset: 4, totalCount: 10);
+      DataList<int> list3 =
+          DataList(data: [1, 2, 4, 5], limit: 5, offset: 2, totalCount: 10);
+      list1.merge(list2);
+      expect(list1, list3);
+    });
+  }
+
+  static void checkMergeWithCollision3() {
+    test('checkMergeWithCollision3', () {
+      DataList<int> list1 =
+          DataList(data: [1, 2, 3, 4, 5], limit: 5, offset: 0, totalCount: 10);
+      DataList<int> list2 =
+          DataList(data: [6, 7], limit: 3, offset: 0, totalCount: 10);
+      DataList<int> list3 =
+          DataList(data: [6, 7], limit: 3, offset: 0, totalCount: 10);
+      list1.merge(list2);
+      expect(list1, list3);
+    });
+  }
+
+  static void checkMergeWithCollision4() {
+    test('checkMergeWithCollision4', () {
+      DataList<int> list1 =
+          DataList(data: [6, 7], limit: 3, offset: 0, totalCount: 10);
+      DataList<int> list2 =
+          DataList(data: [1, 2, 3, 4, 5], limit: 5, offset: 0, totalCount: 10);
+      DataList<int> list3 =
+          DataList(data: [1, 2, 3, 4, 5], limit: 5, offset: 0, totalCount: 10);
+      list1.merge(list2);
+      expect(list1, list3);
+    });
+  }
+
+  static void checkMergeEmptyWithNormal() {
+    test('checkMergeEmptyWithNormal', () {
+      DataList<int> list1 = DataList.empty();
+      DataList<int> list2 =
+          DataList(data: [1, 2, 3, 4, 5], limit: 5, offset: 0, totalCount: 10);
+      DataList<int> list3 =
+          DataList(data: [1, 2, 3, 4, 5], limit: 5, offset: 0, totalCount: 10);
+
+      list1.merge(list2);
+
+      expect(list3, list1);
+    });
+  }
+
+  static void checkInvalidData() {
+    test('checkInvalidData', () {
+      DataList<int> list1 =
+          DataList(data: [1, 2, 3], limit: 3, offset: 10, totalCount: 10);
+      DataList<int> list2 =
+          DataList(data: [4, 5], limit: 3, offset: 4, totalCount: 10);
+      try {
+        list1.merge(list2);
+      } on IncompatibleRangesException catch (e) {
+        expect(e.message, 'incorrect data range');
+      }
+    });
+  }
+
+  static void transformToStringDataList() {
+    test('transformToStringDataList', () {
+      DataList<int> intList =
+          DataList(data: [1, 2, 3, 4, 5], limit: 5, offset: 0, totalCount: 5);
+      DataList<String> stringList = intList.transform((i) => i.toString());
+
+      DataList<String> expectedList = DataList(
+          data: ["1", "2", "3", "4", "5"], limit: 5, offset: 0, totalCount: 5);
+      expect(expectedList, stringList);
+    });
+  }
+
+// map isn't implemented
+/*   static void checkMapExtensionDataList() {
+    test('checkMapExtensionDataList', () {
+      DataList<int> intList =
+          DataList(data: [1, 2, 3, 4, 5], limit: 5, offset: 0, totalCount: 5);
+      DataList<String> stringList = intList.map((i) => i.toString());
+
+      DataList<String> expectedList = DataList(
+          data: ["1", "2", "3", "4", "5"], limit: 5, offset: 0, totalCount: 5);
+      expect(expectedList, stringList);
+    });
+  } */
+
+  static void checkFilterExtensionDataList() {
+    test('checkFilterExtensionDataList', () {
+      DataList<int> negativeList = DataList(
+          data: [-1, -2, -3, -4, -5], limit: 5, offset: 0, totalCount: 5);
+      DataList<int> filteredList = negativeList.where((i) => i > 0);
+
+      DataList<int> emptyList = DataList<int>.empty();
+      expect(emptyList.length, filteredList.length);
+    });
+  }
+
+  static void checkDynamicDataInsertion() {
+    test('checkDynamicDataInsertion', () {
+      final element1 = Element(1);
+      final element2 = Element(2);
+      final element3 = Element(3);
+      final element4 = Element(4);
+      final element5 = Element(5);
+      final element6 = Element(6);
+      final element7 = Element(7);
+      final element8 = Element(8);
+
+      DataList<Element> list1 = DataList(
+          data: [element1, element2, element3, element4, element5],
+          limit: 5,
+          offset: 4,
+          totalCount: 20);
+
+      DataList<Element> list2 = DataList(
+          data: [element4, element5, element6, element7, element8],
+          limit: 5,
+          offset: 9,
+          totalCount: 10);
+
+      list1.mergeWithPredicate(list2, (element) => element.id);
+
+      DataList<Element> list3 = DataList(data: [
+        element1,
+        element2,
+        element3,
+        element4,
+        element5,
+        element6,
+        element7,
+        element8
+      ], limit: 10, offset: 4, totalCount: 20);
+      expect(list3, list1);
+    });
+  }
+
+  static void checkDynamicDataInsertion2() {
+    test('checkDynamicDataInsertion2', () {
+      final element1 = Element(1);
+      final element2 = Element(2);
+      final element3 = Element(3);
+      final element4 = Element(4);
+      final element5 = Element(5);
+      final element6 = Element(6);
+      final element7 = Element(7);
+
+      DataList<Element> list1 = DataList(data: [
+        element1,
+        element2,
+        element3,
+        element4,
+        element5,
+        element6,
+        element7
+      ], limit: 7, offset: 6, totalCount: 30);
+
+      DataList<Element> list2 = DataList(data: [
+        element1,
+        element2,
+        element3,
+        element4,
+        element5,
+        element6,
+        element7
+      ], limit: 7, offset: 13, totalCount: 30);
+
+      DataList<Element> list3 = DataList(data: [
+        element1,
+        element2,
+        element3,
+        element4,
+        element5,
+        element6,
+        element7
+      ], limit: 7, offset: 20, totalCount: 30);
+
+      list1.mergeWithPredicate(list2, (element) => element.id);
+      list1.mergeWithPredicate(list3, (element) => element.id);
+
+      DataList<Element> list4 = DataList(data: [
+        element1,
+        element2,
+        element3,
+        element4,
+        element5,
+        element6,
+        element7
+      ], limit: 21, offset: 6, totalCount: 30);
+
+      expect(list1, list4);
+    });
+  }
+
+  static void checkDynamicDataInsertion3() {
+    test('checkDynamicDataInsertion3', () {
+      final element0 = Element(0);
+      final element1 = Element(1);
+      final element2 = Element(2);
+      final element3 = Element(3);
+      final element4 = Element(4);
+      final element5 = Element(5);
+      final element6 = Element(6);
+      final element7 = Element(7);
+      final element8 = Element(8);
+      final element9 = Element(9);
+
+      DataList<Element> list1 = DataList(
+          data: [element1, element2, element3, element4, element5],
+          limit: 5,
+          offset: 4,
+          totalCount: 30);
+
+      DataList<Element> list2 = DataList(
+          data: [element0, element1, element2, element3, element4],
+          limit: 5,
+          offset: 9,
+          totalCount: 30);
+
+      DataList<Element> list3 = DataList(
+          data: [element5, element6, element7, element8, element9],
+          limit: 5,
+          offset: 14,
+          totalCount: 30);
+
+      list1.mergeWithPredicate(list2, (element) => element.id);
+      list1.mergeWithPredicate(list3, (element) => element.id);
+
+      DataList<Element> list4 = DataList(data: [
+        element1,
+        element2,
+        element3,
+        element4,
+        element5,
+        element0,
+        element6,
+        element7,
+        element8,
+        element9
+      ], limit: 15, offset: 4, totalCount: 30);
+
+      expect(list1, list4);
+    });
+  }
+}
+
+class Element {
+  final int id;
+
+  Element(this.id);
+
+  @override
+  String toString() => "Element: $id";
+}
