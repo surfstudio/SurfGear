f4ee2e27d6709580ae4f890ab33d6c95d6c1550f
zaytsev <zaytsev@surfstudio.ru>
2019-09-18 16:23:30 +0300

    mwwm widget experiment

--- mwwm/lib/src/mwwm_widget.dart
+++ mwwm/lib/src/mwwm_widget.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/widgets.dart';
+import 'package:injector/injector.dart';
+import 'package:mwwm/src/widget_model.dart';
 
-class MWWMWidget extends StatefulWidget {
+typedef WidgetModelBuilder<C> = WidgetModel Function(BuildContext, C);
+
+class MWWMWidget<C extends Component> extends StatefulWidget {
+  final C component;
+  final WidgetModelBuilder<C> wmBuilder;
+  final Widget child;
+
+  const MWWMWidget({
+    Key key,
+    this.component,
+    this.wmBuilder,
+    this.child,
+  }) : super(key: key);
+
   @override
   _MWWMWidgetState createState() => _MWWMWidgetState();
 }
 
 class _MWWMWidgetState extends State<MWWMWidget> {
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Injector(
+      component: widget.component,
+      builder: (context) {
+          return widget.child;
+      },
+    );
   }
 }
--- mwwm/lib/src/widget_state.dart
+++ mwwm/lib/src/widget_state.dart
@@ -15,6 +15,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:injector/injector.dart';
 import 'package:mwwm/src/di/base_wm_component.dart';
+import 'package:mwwm/src/mwwm_widget.dart';
 import 'package:mwwm/src/widget_model.dart';
 
 abstract class WidgetState<T extends StatefulWidget, WM extends WidgetModel,
@@ -30,38 +31,33 @@ abstract class WidgetState<T extends StatefulWidget, WM extends WidgetModel,
 
   Widget buildState(BuildContext context);
 
-  C getComponent(BuildContext context);
-
   @override
   void initState() {
     super.initState();
     print("DEV_INFO init State $this");
+    _attachWidgetModel(context);
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     print("DEV_INFO didChangeDependencies State $this");
-    if (_component == null) {
-      _component = getComponent(context);
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
     print("DEV_INFO $this rebuild");
-    return Injector(
-      component: _component,
-      builder: (context) {
-        _attachWidgetModel(context);
-        return buildState(context);
-      },
-    );
+    return buildState(context);
   }
 
   void _attachWidgetModel(BuildContext context) {
-    var debugWm = Injector.of<C>(context).component.wm;
+    var debugWm = (context
+            .ancestorStateOfType(
+              TypeMatcher<MWWMWidget>(),
+            )
+            .widget as MWWMWidget)
+        .wmBuilder(context, _component);
     print("DEV_INFO identical wm ${wm == debugWm}");
     wm ??= debugWm;
   }
