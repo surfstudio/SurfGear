71b65ac4ab810c7a881a7462c85bc36aca4e2c23
Aleksey Radionov <radionov@surfstudio.co>
2019-10-02 10:43:34 +0300

    ANDDEP-740 - add doc

--- surf_util/lib/src/ui/widget/disable_overscroll_widget.dart
+++ surf_util/lib/src/ui/widget/disable_overscroll_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+/// place the [SingleChildScrollView] inside the [DisableOverscroll] widget
+/// to prevent glowing when scrolling over the edge
 class DisableOverscroll extends StatelessWidget {
   const DisableOverscroll({
     Key key,
