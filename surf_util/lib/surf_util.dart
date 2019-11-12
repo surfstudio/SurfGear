6c3713e403c0347de5f156e810d5cc1434f240be
Korpatenkov <korpatenkov@surfstudio.co>
2019-11-12 12:09:49 +0300

    KDB-2783 add surf_util package

---
+++ surf_util/lib/surf_util.dart
@@ -0,0 +1,6 @@
+library surf_util;
+
+export 'package:surf_util/src/enum.dart';
+export 'package:surf_util/src/res/consts.dart';
+export 'package:surf_util/src/transformable.dart';
+export 'package:surf_util/src/ui/widget/disable_overscroll_widget.dart';
