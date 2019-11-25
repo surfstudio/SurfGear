9b37aa89e628ffb23a9a31cd8bf6e572d8869be3
mjk <mbixjkee1392@gmail.com>
2019-11-25 14:27:10 +0300

    ANDDEP-730. Test covering.

--- bottom_navigation_bar/test/bottom_navigation_bar_test.dart
+++ bottom_navigation_bar/test/bottom_navigation_bar_test.dart
@@ -1,2 +1,134 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:bottom_navigation_bar/bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+Key _greenKey = Key("greenButton");
+Key _redKey = Key("redButton");
+Key _blueKey = Key("blueButton");
+
+List<BottomNavTabType> _types = [
+  BottomNavTabType(0),
+  BottomNavTabType(1),
+  BottomNavTabType(2),
+];
+
+Map<BottomNavTabType, BottomNavigationRelationship> _map = {
+  _types[0]: BottomNavigationRelationship(
+    () => _buildPage("Red"),
+    (isSelected) => _buildButton(
+      _redKey,
+      isSelected,
+      Color(0x55FF0000),
+    ),
+  ),
+  _types[1]: BottomNavigationRelationship(
+    () => _buildPage("Green"),
+    (isSelected) => _buildButton(
+      _greenKey,
+      isSelected,
+      Color(0x5500FF00),
+    ),
+  ),
+  _types[2]: BottomNavigationRelationship(
+    () => _buildPage("Blue"),
+    (isSelected) => _buildButton(
+      _blueKey,
+      isSelected,
+      Color(0x550000FF),
+    ),
+  ),
+};
+
+Widget _buildButton(Key key, bool isSelected, Color color) {
+  return Container(
+    key: key,
+    height: 100,
+    color: color,
+    child: isSelected
+        ? Center(
+            child: Icon(Icons.check),
+          )
+        : Container(),
+  );
+}
+
+Widget _buildPage(String color) {
+  return Container(
+    child: Text(color),
+  );
+}
+
 void main() {
+  setUp(() {});
+
+  tearDown(() {});
+
+  redButtonTest();
+  greenButtonTest();
+  blueButtonTest();
+  outerActionTest();
+}
+
+
+void redButtonTest() {
+  testWidgets('Red tap', (WidgetTester tester) async {
+    await _buttonTest(_redKey, "Red", tester);
+  });
+}
+
+void greenButtonTest() {
+  testWidgets('Green tap', (WidgetTester tester) async {
+    await _buttonTest(_greenKey, "Green", tester);
+  });
+}
+
+void blueButtonTest() {
+  testWidgets('Blue tap', (WidgetTester tester) async {
+    await _buttonTest(_blueKey, "Blue", tester);
+  });
+}
+
+void outerActionTest() {
+  testWidgets('Outer action', (WidgetTester tester) async {
+    var sc = StreamController<BottomNavTabType>.broadcast();
+
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: BottomNavigator(
+          initialTab: _types[0],
+          tabsMap: _map,
+          selectController: sc,
+        ),
+      ),
+    ));
+
+    sc.sink.add(_types[2]);
+
+    await tester.pump();
+
+    expect(find.text("Blue"), findsOneWidget);
+
+    sc.close();
+  });
+}
+
+Future<void> _buttonTest(Key key, String result, WidgetTester tester) async {
+  await tester.pumpWidget(MaterialApp(
+    home: Scaffold(
+      body: BottomNavigator(
+        initialTab: _types[0],
+        tabsMap: _map,
+        selectController: StreamController<BottomNavTabType>.broadcast(),
+      ),
+    ),
+  ));
+
+  await tester.tap(find.byKey(key).first);
+
+  await tester.pump();
+
+  expect(find.text(result), findsOneWidget);
 }
