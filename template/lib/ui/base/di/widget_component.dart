9793b4281de15c74aab11fc441f932942e8c0163
Zotyev <zotev@surfstudio.ru>
2019-12-24 15:06:07 +0300

    ANDDEP-830. Base widget component.

---
+++ template/lib/ui/base/di/widget_component.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/ui/app/di/app.dart';
+import 'package:flutter_template/ui/base/default_dialog_controller.dart';
+import 'package:flutter_template/ui/base/error/standard_error_handler.dart';
+import 'package:flutter_template/ui/base/material_message_controller.dart';
+import 'package:injector/injector.dart';
+import 'package:mwwm/mwwm.dart';
+
+/// Base component with common dependencies.
+abstract class WidgetComponent implements Component {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  MessageController messageController;
+  DialogController dialogController;
+  NavigatorState navigator;
+  WidgetModelDependencies wmDependencies;
+
+  WidgetComponent(BuildContext context) {
+    var appComponent = Injector.of<AppComponent>(context).component;
+
+    messageController = MaterialMessageController(scaffoldKey);
+    dialogController = DefaultDialogController(scaffoldKey);
+    navigator = Navigator.of(context);
+
+    wmDependencies = WidgetModelDependencies(
+      errorHandler: StandardErrorHandler(
+        messageController,
+        dialogController,
+        appComponent.scInteractor,
+      ),
+    );
+  }
+}
