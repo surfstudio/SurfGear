e122ad24ee8e28c149fb1266928607e7748c8314
Zotyev <zotev@surfstudio.ru>
2019-12-25 15:34:32 +0300

    ANDDEP-832. Проверяем версию флатера перед тем как просим ее поменять.

--- template/script/version_impl/checkout_version.dart
+++ template/script/version_impl/checkout_version.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:args/args.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   exitCode = 0;
   final parser = ArgParser();
 
@@ -13,9 +13,22 @@ void main(List<String> arguments) {
   } else {
     var version = args[0];
 
-    Process.run('flutter', ['version', version]).then((result) {
-      stdout.write(result.stdout);
-      stderr.write(result.stderr);
-    });
+    var checkVersion = await Process.run('flutter', ['--version', '--machine']);
+
+    var needChangeVersion = true;
+
+    // если запрос завершился ошибкой тогда все равно попробуем сменить версию
+    if (checkVersion.stderr.toString().isEmpty) {
+      needChangeVersion = !checkVersion.stdout
+          .toString()
+          .contains('"frameworkVersion": "$version"');
+    }
+
+    if (needChangeVersion) {
+      Process.run('flutter', ['version', version]).then((result) {
+        stdout.write(result.stdout);
+        stderr.write(result.stderr);
+      });
+    }
   }
 }
