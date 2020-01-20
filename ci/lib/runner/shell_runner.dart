fbbd256b14c003cc9956382f80ab15992930a39c
Zotyev <zotev@surfstudio.ru>
2020-01-20 18:15:43 +0300

    FLT-11.Runner for console commands added.

---
+++ ci/lib/runner/shell_runner.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:shell/shell.dart';
+
+/// Глобальный аллиас для запуска команд.
+Future<ProcessResult> sh(String command, [List<String> arguments]) {
+  return ShellRunner.run(command, arguments);
+}
+
+/// Утилита запуска консольных команд.
+class ShellRunner {
+  static final _shell = Shell();
+
+  /// Запускает переданную команду на выполнение.
+  static Future<ProcessResult> run(String command, [List<String> arguments]) {
+    arguments = arguments ?? <String>[];
+    return _shell.run(command, arguments);
+  }
+}
