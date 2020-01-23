8a18a6b650825159c6b900f9318838d2dba242a6
osipov-e <osipov-e@surfstudio.ru>
2020-01-23 12:26:50 +0300

    FLT-129 Доки

--- ci/lib/tasks/pub_dry_task.dart
+++ ci/lib/tasks/pub_dry_task.dart
@@ -5,6 +5,7 @@ import 'package:ci/exceptions/exceptions.dart';
 import 'package:ci/runner/shell_runner.dart';
 import 'package:ci/helper/process_result_extension.dart';
 
+/// Проверка на возможность публикации пакета  модулей openSource
 class DryRunTask {
   Future<void> run(List<Element> elements) async {
     final messages = [];
@@ -24,10 +25,12 @@ class DryRunTask {
     }
   }
 
+  /// Ждём результата провеки
   Future<ProcessResult> _getProcessResult(Element element) {
     return sh('pub publish --dry-run', path: element.path);
   }
 
+  /// Выводим список ошибок
   void _printMessages(List<String> messages) {
     throw ModuleNotReadyForOpenSours(messages.join('\n'));
   }
