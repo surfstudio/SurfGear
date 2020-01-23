9a82fd903a4c0674595a9db85ebba19a2954f52c
Антон Докусов <dokusov@surfstudio.co>
2020-01-23 11:28:59 +0300

    FLT-128 Удалил тест

--- ci/test/checks_test.dart
+++ ci/test/checks_test.dart
@@ -54,20 +54,4 @@ void main() {
       returnsNormally,
     );
   });
-
-  test('find changed module', () async {
-    final file = File('../packages/auto_reload/lib/auto_reload.dart');
-    file.writeAsStringSync(' ', mode: FileMode.append, flush: true);
-
-    await sh('git add ../packages/auto_reload/lib/auto_reload.dart');
-    await sh('git commit -m "findchangestest"');
-
-    final element = Element(path: 'auto_reload');
-    final changedElements = await findChangedElements([element]);
-
-    expect(changedElements[0] == element, true);
-
-    await sh('git reset HEAD~');
-    await sh('git checkout -- ../packages/auto_reload/lib/auto_reload.dart');
-  });
 }
