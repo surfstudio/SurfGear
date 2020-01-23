b63355a9f8a41676db57fbbbd2c74e2ea448c785
osipov-e <osipov-e@surfstudio.ru>
2020-01-23 12:01:43 +0300

    FLT-129 реализовал проверку [--dry-run] модулей openSource

---
+++ ci/lib/tasks/pub_dry_task.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:ci/domain/element.dart';
+import 'package:ci/exceptions/exceptions.dart';
+import 'package:ci/runner/shell_runner.dart';
+import 'package:ci/helper/process_result_extension.dart';
+
+class DryRunTask {
+  Future<void> run(List<Element> elements) async {
+    final messages = [];
+
+    for (var element in elements) {
+      if (element.hosted) {
+        final result = await _getProcessResult(element);
+        result.print();
+        if (result.exitCode != 0) {
+          messages
+              .add(element.name.toString() + ' ' + result.stderr.toString());
+        }
+      }
+    }
+    if (messages.isNotEmpty) {
+      _printMessages(messages);
+    }
+  }
+
+  Future<ProcessResult> _getProcessResult(Element element) {
+    return sh('pub publish --dry-run', path: element.path);
+  }
+
+  void _printMessages(List<String> messages) {
+    throw ModuleNotReadyForOpenSours(messages.join('\n'));
+  }
+}
