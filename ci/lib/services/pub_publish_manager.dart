a43688db8025a4d6adc07a7b9869f46f9db1b3d5
osipov-e <osipov-e@surfstudio.ru>
2020-01-24 12:23:29 +0300

    FLT-129 Решил конфликт

--- ci/lib/services/pub_publish_manager.dart
+++ ci/lib/services/pub_publish_manager.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:ci/domain/element.dart';
-import 'package:ci/runner/shell_runner.dart';
+import 'package:ci/services/runner/shell_runner.dart';
 
 /// Для [Element] проверка на возможность публикации пакета
 Future<ProcessResult> runDryPublish(Element element) {
