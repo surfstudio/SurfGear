c21a1de38124ca6b8a4de56444be10a3eca7067c
knightsforce <andrianov44@gmail.com>
2020-02-02 14:19:12 +0200

    ROS Добавил удаление

--- render_metrics/lib/src/manager/render_manager.dart
+++ render_metrics/lib/src/manager/render_manager.dart
@@ -2,5 +2,9 @@ import 'package:flutter/rendering.dart';
 
 /// Базовый класс для менеджеров взаимодействия с рендером
 abstract class RenderManager<T> {
+  /// Обработка вызова добавления RenderObject
   void addRenderObject(T id, RenderObject object);
+
+  /// Обработка вызова удаления RenderObject
+  void removeRenderObject(T id);
 }
