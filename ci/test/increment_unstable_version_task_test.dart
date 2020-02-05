10a069d9b366ff6deed2f8d3ac45a9bc20c2957d
osipov-e <osipov-e@surfstudio.ru>
2020-02-05 19:13:50 +0300

    FLT-140 Рефакторинг

--- ci/test/increment_unstable_version_task_test.dart
+++ ci/test/increment_unstable_version_task_test.dart
@@ -5,44 +5,49 @@ import 'core/test_helper.dart';
 
 void main() {
   group(
-    'Check task to increase unstable version.',
+    'Check task to increase unstable version:',
     () {
       test(
-        'If the library is not ready to be used in production, task should return 1.',
+        'The module is ready for use in production.',
         () async {
-          var res = await _testReturnPreparedTask(isStable: false).run();
-          expect(res.unstableVersion, 1);
-        },
-      );
-
-      test(
-        'If the library was changed as part of the pull request, task should return 1.',
-        () async {
-          var res = await _testReturnPreparedTask(isChanged: true).run();
-          expect(res.unstableVersion, 1);
-        },
-      );
-
-      test(
-        'If the library was changed as part of the pull request and if the library is not ready to be used in production, task should return 1.',
-        () async {
-          var res = await _testReturnPreparedTask(isChanged: true, isStable: false).run();
-          expect(res.unstableVersion, 1);
-        },
-      );
-
-      test(
-        'If the library has not been modified as part of the retrieval request and if the library is ready for use in a production environment, the task should return 0.',
-        () async {
-          var res = await _testReturnPreparedTask(isChanged: false, isStable: true).run();
+          var res = await _prepareTestTask(isChanged: false, isStable: true).run();
           expect(res.unstableVersion, 0);
+        },
+      );
+    },
+  );
+
+  group(
+    'A module that is not ready for production throws an error:',
+    () {
+      test(
+        'Not ready to be used in production.',
+        () async {
+          var res = await _prepareTestTask(isStable: false).run();
+          expect(res.unstableVersion, 1);
+        },
+      );
+
+      test(
+        'The module was changed.',
+        () async {
+          var res = await _prepareTestTask(isChanged: true).run();
+          expect(res.unstableVersion, 1);
+        },
+      );
+
+      test(
+        'The module was changed and not ready to be used in production.',
+        () async {
+          var res = await _prepareTestTask(isChanged: true, isStable: false).run();
+          expect(res.unstableVersion, 1);
         },
       );
     },
   );
 }
 
-IncrementUnstableVersionTask _testReturnPreparedTask({bool isChanged = false, bool isStable = true}) {
+IncrementUnstableVersionTask _prepareTestTask({bool isChanged = false, bool isStable = true}) {
   var element = createTestElement(isChanged: isChanged, isStable: isStable);
   return IncrementUnstableVersionTask(element);
 }
