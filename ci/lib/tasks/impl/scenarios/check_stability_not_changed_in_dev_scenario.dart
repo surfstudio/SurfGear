3d3801d74695694c718eded55eb75968e55966d1
Zotyev <zotev@surfstudio.ru>
2020-02-05 16:39:49 +0300

    FLT-155. Сценарий проверки, что стабильность не поменялась.

---
+++ ci/lib/tasks/impl/scenarios/check_stability_not_changed_in_dev_scenario.dart
@@ -0,0 +1,27 @@
+import 'package:ci/domain/command.dart';
+import 'package:ci/domain/config.dart';
+import 'package:ci/services/parsers/pubspec_parser.dart';
+import 'package:ci/tasks/checks.dart';
+import 'package:ci/tasks/core/task.dart';
+
+/// Сценарий для команды check_stability_not_changed.
+///
+/// Пример вызова:
+/// dart ci check_stability_not_changed
+class CheckStabilityNotChangedInDevScenario extends Scenario {
+  static const String commandName = 'check_stability_not_changed';
+
+  final PubspecParser _pubspecParser;
+
+  CheckStabilityNotChangedInDevScenario(Command command, this._pubspecParser)
+      : super(command);
+
+  @override
+  Future<void> run() async {
+    /// получаем все элементы
+    var elements = _pubspecParser.parsePubspecs(Config.packagesPath);
+
+    /// проверяем, что не поменялась стабильность модулей
+    await checkStabilityNotChangeInDev(elements);
+  }
+}
