3def7055cb61c393fa7a9e36bc494477f429a031
Антон Докусов <dokusov@surfstudio.co>
2020-03-10 12:05:59 +0300

    FLT-194 Исправлено создание списка изменённых файлов

--- tools/ci/lib/tasks/utils.dart
+++ tools/ci/lib/tasks/utils.dart
@@ -115,19 +115,16 @@ Future<void> createChangedListFile(
 
   print('Файлы, изменённые в сравнении с целевой веткой :\n$diff');
 
-  var list = elements.map(
-    (e) {
-      if (diff.contains(e.directoryName)) {
-        e.changed = true;
-      }
+  final changedList = elements
+      .where((e) => diff.contains(e.directoryName))
+      .toList()
+        ..forEach((e) => e.changed = true);
 
-      return e.name;
-    },
-  ).join('\n');
+  final names = changedList.map((e) => e.name).join('\n');
 
-  print('Модули были изменены:\n$list');
+  print('Модули были изменены:\n$names');
 
-  await File(_getChangedListFilePath()).writeAsString(list);
+  await File(_getChangedListFilePath()).writeAsString(names);
 }
 
 /// Удаляет файл со списком измененных файлов.
