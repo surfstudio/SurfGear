fb97a2470b5e090906d2697758e4e76703644ca6
Korpatenkov <korpatenkov@surfstudio.co>
2020-03-11 13:36:58 +0300

    no-task rm typed methods from dialog_controller

--- mwwm/lib/src/controller/dialog_controller.dart
+++ mwwm/lib/src/controller/dialog_controller.dart
@@ -25,8 +25,4 @@ abstract class DialogController {
     void Function(BuildContext context) onAgreeClicked,
     void Function(BuildContext context) onDisagreeClicked,
   });
-
-  Future<R> showSheet<R>(dynamic type, {VoidCallback onDismiss});
-
-  Future<R> showModalSheet<R>(dynamic type);
 }
