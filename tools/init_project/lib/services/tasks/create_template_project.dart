d6d3f0cd04cd33588e80ea78bd090f855029b8bc
osipov-e <osipov-e@surfstudio.ru>
2020-03-11 20:15:21 +0300

    FLT-157 проходим по всем файлам, но пока не трогаим их

--- tools/init_project/lib/services/tasks/create_template_project.dart
+++ tools/init_project/lib/services/tasks/create_template_project.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:init_project/domain/path_directory.dart';
 import 'package:init_project/services/manager/message_console_manager.dart';
 import 'package:io/io.dart';
@@ -5,6 +7,8 @@ import 'package:path/path.dart' as p;
 
 /// Путь до папки с template
 const String _pathPackagesTemplate = 'packages/template';
+const String _fileName = '.dart';
+RegExp _expDependency = RegExp('template');
 
 /// Создает шаблонный проект
 class CreateTemplateProject {
@@ -17,7 +21,7 @@ class CreateTemplateProject {
   Future<void> run(PathDirectory pathDirectory) async {
     try {
       await _copyTemplateFolder(pathDirectory);
-      await _renameFile();
+      await _renameFile(pathDirectory);
     } catch (e) {
       rethrow;
     }
@@ -29,7 +33,30 @@ class CreateTemplateProject {
     await copyPath(pathFolder, pathDirectory.path);
   }
 
-  Future<void> _renameFile() async {
+  Future<void> _renameFile(PathDirectory pathDirectory) async {
+    var dir = Directory(pathDirectory.path);
     _showMessageConsole.printMessageConsole('rename file');
+    await dir.list(recursive: true, followLinks: false).listen((FileSystemEntity entity) async {
+      String fileName = p.basename(entity.path);
+
+      if (await FileSystemEntity.isFile(entity.path)) {
+        if (fileName.contains(_fileName)) {
+          var tempFile = File(entity.path);
+          List<String> r = tempFile.readAsLinesSync();
+          for (var _r in r){
+            _r.replaceAllMapped(_expDependency,( replace){
+              return '"${replace.group(0)}"';
+            });
+          }
+//          print(r);
+        }
+      }
+    });
+
+//    final string = '{name : aName, hobby : [fishing, playing_guitar]}';
+//    final newString = string.replaceAllMapped(RegExp(r'\b\w+\b'), (match) {
+//      return '"${match.group(0)}"';
+//    });
+//    print(newString);
   }
 }
