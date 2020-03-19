c31eb7544e60a0740545bcf4be32058be28f0211
osipov-e <osipov-e@surfstudio.ru>
2020-03-20 01:57:13 +0300

    FLT-181 вариант с хорошим ветвлением

--- tools/ci/lib/scenarios/show_dependency_graph.dart
+++ tools/ci/lib/scenarios/show_dependency_graph.dart
@@ -4,7 +4,8 @@ import 'package:ci/services/parsers/command_parser.dart';
 import 'package:ci/services/parsers/pubspec_parser.dart';
 import 'package:ci/tasks/core/scenario.dart';
 
-const String _arrowDep = ' |-> ';
+const String _arrowDep = ' | ';
+const String _arrow = ' ---->';
 
 ///  Выводит в консоль граф зависимостей елемента
 ///
@@ -20,39 +21,32 @@ class ShowDependencyGraph extends Scenario {
 
   @override
   Future<void> doExecute(List<Element> elements) async {
-    /// Содержит мапу с зависимостями елементов
-    var mapGraph = <Element>[];
-
     /// Хранит данные для вывода в консоль
     final str = StringBuffer();
-    var el = <Element>[];
-//    el.add(elements[0]);
-//    el.add(elements[1]);
-//    el.add(elements[2]);
-//    el.add(elements[3]);
-//    el.add(elements[4]);
-//    el.add(elements[5]);
-    el.add(elements[7]);
-    el.add(elements[6]);
-//    el.add(elements[0]);
-//    el.add(elements[8]);
-    el.add(elements[10]);
-    el.add(elements[11]);
-    _test(elements, str);
+
+    for (var element in elements) {
+      str.write('\n');
+      _test(element, str, -_arrowDep.length);
+    }
     print(str);
-
-
   }
 
-  void _test(List<Element> elements, StringBuffer str, [int parentLength = 0]) {
-    var length = _getLengthMaxName(elements) + parentLength;
-    for (var element in elements) {
-      str.write('\n' + element.name);
-      if (_isDependency(element)) {
-        var space = _getLine(length, element);
-        str.write(space);
-        print(length + space.length);
-        _dep(element, str, length + _arrowDep.length - 1);
+  void _test(Element element, StringBuffer str, int length) {
+    str.write(element.name);
+    if (_isDependency(element)) {
+      str.write(_arrow);
+      _dep(element, str, length + element.name.length + _arrow.length + _arrowDep.length);
+    }
+  }
+
+  void _dep(Element element, StringBuffer str, int length) {
+    var dependencies = element.dependencies;
+    for (var i = 0; i < dependencies.length; i++) {
+      var dep = dependencies[i];
+
+      if (!dep.thirdParty) {
+        i == 0 ? str.write(_arrowDep) : str.write('\n' + ' ' * length + '$_arrowDep');
+        _test(dep.element, str, length);
       }
     }
   }
@@ -64,37 +58,6 @@ class ShowDependencyGraph extends Scenario {
       }
     }
     return false;
-  }
-
-  void _dep(Element element, StringBuffer str, int length) {
-    var deps = element.dependencies;
-    var depElement = <Element>[];
-    for (var i = 0; i < deps.length; i++) {
-      var dep = deps[i];
-
-      if (!dep.thirdParty) {
-        i == 0
-            ? str.write(_arrowDep + dep.element.name)
-            : str.write('\n' + ' ' * length + '$_arrowDep' + dep.element.name);
-        _dep(dep.element, str, length);
-      }
-    }
-  }
-
-  /// Возвращает максимальную длину имени из списка имен
-  int _getLengthMaxName(List<Element> elements) {
-    var maxLength = 0;
-    for (var element in elements) {
-      if (element.name.length > maxLength) {
-        maxLength = element.name.length;
-      }
-    }
-    return maxLength;
-  }
-
-  String _getLine(int maxLength, Element element) {
-    var difference = maxLength - element.name.length;
-    return '-' * difference + '--->';
   }
 
   @override
