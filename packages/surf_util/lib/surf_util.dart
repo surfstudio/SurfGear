b3ae6f44ef657d3b05038a4edabe42766da6b515
zotyev <zotev@surfstudio.ru>
2020-04-10 11:13:16 +0300

    FLT-196. Add bitmask

--- packages/surf_util/lib/surf_util.dart
+++ packages/surf_util/lib/surf_util.dart
@@ -1,6 +1,7 @@
 library surf_util;
 
-export 'package:surf_util/src/enum.dart';
+export 'package:surf_util/src/enum/enum.dart';
+export 'package:surf_util/src/enum/bitmask.dart';
 export 'package:surf_util/src/res/consts.dart';
 export 'package:surf_util/src/transformable.dart';
 export 'package:surf_util/src/ui/widget/disable_overscroll_widget.dart';
