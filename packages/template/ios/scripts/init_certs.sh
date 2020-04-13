da530013e2ba8eb693241c6b41826af302fa4aef
zotyev <zotev@surfstudio.ru>
2020-04-13 17:19:30 +0300

    FLT-219.Fix template script, check for exist directory added.

--- packages/template/ios/scripts/init_certs.sh
+++ packages/template/ios/scripts/init_certs.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 # Add certificates to keychain and allow codesign to access them
-security import ./certs/ios_development.cer -T /usr/bin/codesign #todo название файла сертификата
-# security import ./certs/dev.p12 -P "" -T /usr/bin/codesign
-security import ./certs/ios_distribution.cer -T /usr/bin/codesign #todo название файла сертификата
-# security import ./certs/distr.p12 -P "" -T /usr/bin/codesign
+security import ./certs/ios_development.cer -P "" -T /usr/bin/codesign #todo название файла сертификата
+security import ./certs/ios_distribution.cer -P "" -T /usr/bin/codesign #todo название файла сертификата
 
 output=~/Library/MobileDevice/Provisioning\ Profiles
 
+if [[ ! -e "$output" ]]; then
+    mkdir -p "$output"
+fi
+
 cp -R  ./certs/YOUR_PROVISIONING_PROFILE_DEV.mobileprovision "${output}" #todo название файла PP
-# cp -R  ./certs/iOS_Prod_Development.mobileprovision "${output}"
 cp -R  ./certs/YOUR_PROVISIONING_PROFILE_DISTRIBUTION.mobileprovision "${output}"#todo название файла PP
