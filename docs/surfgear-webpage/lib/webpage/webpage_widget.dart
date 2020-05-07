721f65d4a0632d44b055b990d3f591e5c51389e6
belenkov <belenkov@surfstudio.ru>
2020-05-07 15:32:30 +0500

    pr edits

--- docs/surfgear-webpage/lib/webpage/webpage_widget.dart
+++ docs/surfgear-webpage/lib/webpage/webpage_widget.dart
@@ -38,7 +38,6 @@ class _WebpageWidgetState extends State<WebpageWidget>
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     _pageOffsetController.close();
   }
