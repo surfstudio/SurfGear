f8043d8f79bed9986d9257fb2e0092a92615edb9
dmitrijandrianov <andriyanov@surfstudio.ru>
2020-06-23 16:57:39 +0200

    FLT-255 Сверка версий с pub

--- tools/ci/lib/scenarios/publish_modules_scenario.dart
+++ tools/ci/lib/scenarios/publish_modules_scenario.dart
@@ -2,6 +2,7 @@ import 'package:ci/domain/command.dart';
 import 'package:ci/domain/element.dart';
 import 'package:ci/exceptions/exceptions.dart';
 import 'package:ci/exceptions/exceptions_strings.dart';
+import 'package:ci/services/managers/version_manager.dart';
 import 'package:ci/services/parsers/pubspec_parser.dart';
 import 'package:ci/tasks/core/scenario.dart';
 import 'package:ci/tasks/tasks.dart';
@@ -18,6 +19,7 @@ class PublishModulesScenario extends ChangedElementScenario {
   static const String commandName = 'publish';
   static const String server = 'server';
   static const String helpServer = 'Server for publish module.';
+  static final VersionManager _versionManager = VersionManager();
 
   @override
   Map<String, String> getCommandsHelp() => {
@@ -37,26 +39,44 @@ class PublishModulesScenario extends ChangedElementScenario {
   Future<void> doExecute(List<Element> elements) async {
     var targetServer = command.arguments[server];
     var releaseElements = elements.where((e) => e.isStable).toList();
+    StringBuffer _buffer;
 
     print("Stable packages: ${releaseElements.map((p) => p.name).join(',')}");
     try {
       var failedModulesNames = <String>[];
 
       for (var element in releaseElements) {
+        /// Сверять версию с pub
         try {
+          final isNewVersion = await _versionManager.checkIsNewVersion(
+            element.name,
+            element.version,
+          );
+
+          if (!isNewVersion) {
+            print(
+                'package ${element.name} v${element.version}: the local package has the same version');
+            continue;
+          }
+
           await pubPublishModules(element, pathServer: targetServer);
           print(element.name + ' published');
         } on OpenSourceModuleCanNotBePublishException catch (e) {
+          _buffer ??= StringBuffer('failed packages:');
+          _buffer.write('\n${element.name}');
+
           print(e);
           failedModulesNames.add(element.name);
           continue;
         }
       }
-      
-      if (failedModulesNames.isNotEmpty) {
+
+      if (elements.length == failedModulesNames.length) {
         throw OpenSourceModuleCanNotBePublishException(
           getModuleCannotBePublishedExceptionText(failedModulesNames.join(',')),
         );
+      } else if (_buffer != null) {
+        print(_buffer.toString());
       }
     } on BaseCiException {
       rethrow;
