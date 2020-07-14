6ecc72f729b269bc8254df8b95818408cd65d827
krasikov-surf <krasikov@surfstudio.ru>
2020-07-14 11:58:39 +0300

    FLT-279 review

--- packages/jitsi_meet/android/src/main/kotlin/jitsimeet/jitsi_meet/JitsiMeetFactory.kt
+++ packages/jitsi_meet/android/src/main/kotlin/jitsimeet/jitsi_meet/JitsiMeetFactory.kt
@@ -6,7 +6,9 @@ import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 import io.flutter.plugin.common.BinaryMessenger
 
-class JitsiMeetFactory(private val messanger: BinaryMessenger, private val activityContext: android.content.Context?) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+class JitsiMeetFactory(private val messanger: BinaryMessenger,
+                       lprivate val activityContext: android.content.Context?
+) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
 
     override fun create(context: android.content.Context, i: Int, o: Any?): PlatformView {
         return JitsiMeetController(activityContext ?: context, messanger, i)
