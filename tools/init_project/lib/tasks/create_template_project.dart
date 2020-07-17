e5d711600c686326ecac37ee23b64f340e145dbe
Aleksey Radionov <radionov@surfstudio.ru>
2020-07-17 08:34:49 +0300

    FLT-303 - change bundle id for template project

--- tools/init_project/lib/tasks/create_template_project.dart
+++ tools/init_project/lib/tasks/create_template_project.dart
@@ -22,19 +22,43 @@ RegExp _expDependency = RegExp('template');
 
 String _pubspecFile = 'pubspec.yaml';
 
+const String _bundlePattern = '%bundleId%';
+
+/// android files containing application id
+const Map<String, Map<String, String>> _androidFiles = {
+  '/android/app/build.gradle': {
+    'applicationId "ru.surfstudio.flutterTemplate"':
+        'applicationId "%bundleId%"',
+  },
+};
+
 /// Создает шаблонный проект.
 class CreateTemplateProject {
-  Future<void> run(Command command, PathDirectory pathDirectory) async {
+  Future<void> run(
+    Command command,
+    PathDirectory pathDirectory,
+  ) async {
     try {
       printMessageConsole('Prepare project "${command.nameProject}"...');
 
       await _copyTemplateInFolder(pathDirectory);
-      final files = await _searchFile(pathDirectory);
-      await _replaceTextInFile(command, files);
-      await _searchPubspec(command, files);
+      await _renameDependencies(pathDirectory, command);
+      await _renameBundleId(pathDirectory, command);
     } catch (e) {
       rethrow;
     }
+  }
+
+  String _getBundleId(Command command) =>
+      '${command.organizationId}.${command.nameProject}';
+
+  Future<void> _renameDependencies(
+    PathDirectory pathDirectory,
+    Command command,
+  ) async {
+    final files = await _searchFile(pathDirectory);
+    await _replaceDependencyTextInFile(command, files);
+    await _searchPubspec(command, files);
   }
 
   /// Копируем template.
@@ -50,7 +74,8 @@ class CreateTemplateProject {
     final List<File> files = [];
     final dirProject = Directory(p.join(pathDirectory.path, 'lib'))
         .listSync(recursive: true, followLinks: false)
-          ..addAll(Directory(pathDirectory.path).listSync(recursive: false, followLinks: false));
+          ..addAll(Directory(pathDirectory.path)
+              .listSync(recursive: false, followLinks: false));
 
     final fileSystemEntities = await _getFiles(dirProject);
 
@@ -73,17 +98,50 @@ class CreateTemplateProject {
     return files;
   }
 
+  Future<File> _getFileByName(String fileName) async {
+    return File(fileName);
+  }
+
   /// Перезаписывем текст в файлах, заменяя зависимости.
-  Future<void> _replaceTextInFile(Command command, List<File> files) async {
+  Future<void> _replaceDependencyTextInFile(
+    Command command,
+    List<File> files,
+  ) async {
     for (var file in files) {
       try {
         final sourceText = await file.readAsString();
-        final outText = sourceText.replaceAll(_expDependency, command.nameProject);
+        final outText =
+            sourceText.replaceAll(_expDependency, command.nameProject);
         await file.writeAsString(outText);
       } catch (e) {
         rethrow;
       }
     }
+  }
+
+  /// Перезаписывем текст в файлах.
+  Future<void> _replaceTextInFile(
+    Command command,
+    File file,
+    Map<String, String> patterns,
+  ) async {
+    try {
+      final sourceText = await file.readAsString();
+      var outText = sourceText;
+      patterns.forEach((fromPattern, toPattern) {
+        outText = outText.replaceAll(
+          fromPattern,
+          _resolvePattern(command, toPattern),
+        );
+      });
+      await file.writeAsString(outText);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  String _resolvePattern(Command command, String pattern) {
+    return pattern.replaceAll(_bundlePattern, _getBundleId(command));
   }
 
   /// Ищем pubspec.yaml
@@ -104,11 +162,13 @@ class CreateTemplateProject {
   /// Перезаписываем pubspec.yaml файл
   Future<void> _replacePubspec(Command command, File file) async {
     try {
-      final pubspecYaml = PubspecYaml.loadFromYamlString(file.readAsStringSync());
+      final pubspecYaml =
+          PubspecYaml.loadFromYamlString(file.readAsStringSync());
 
       var replacePubspec = pubspecYaml.copyWith(
           version: Optional('0.0.1+1'),
-          dependencies: _replaceDependencies(command, pubspecYaml.dependencies.toList()));
+          dependencies:
+              _replaceDependencies(command, pubspecYaml.dependencies.toList()));
 
       await file.writeAsStringSync(replacePubspec.toYamlString());
     } catch (e) {
@@ -144,5 +204,32 @@ class CreateTemplateProject {
     SdkPackageDependencySpec sdk,
   }) {
     return path;
+  }
+
+  Future<void> _renameBundleId(
+    PathDirectory pathDirectory,
+    Command command,
+  ) async {
+    await _renameAndroidBundleId(pathDirectory, command);
+    await _renameIosBundleId(command);
+  }
+
+  Future<void> _renameAndroidBundleId(
+    PathDirectory pathDirectory,
+    Command command,
+  ) async {
+    printMessageConsole(
+        'Change organization to "${command.organizationId}" in android project...');
+
+    _androidFiles.forEach((fileName, patterns) async {
+      final file = await _getFileByName(pathDirectory.path + fileName);
+      if (!file.existsSync()) return;
+      await _replaceTextInFile(command, file, patterns);
+    });
+  }
+
+  Future<void> _renameIosBundleId(Command command) async {
+    printMessageConsole(
+        'Change organization to "${command.organizationId}" in ios project...');
   }
 }
