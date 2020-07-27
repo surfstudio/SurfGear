2adcb5051f0d79ef2ad149e2618543d5e2075298
krasikov-surf <krasikov@surfstudio.ru>
2020-07-27 15:20:50 +0300

    FLT-285 review

--- packages/surf_pay/lib/controller/payment_controller.dart
+++ packages/surf_pay/lib/controller/payment_controller.dart
@@ -10,39 +10,43 @@ import 'package:surfpay/data/google_pay_data.dart';
 import 'package:surfpay/data/goole_payment_request.dart';
 import 'package:surfpay/data/payment_status.dart';
 
-const String CHANNEL_NAME = "surfpay";
-const String PAY_APPLE = "apple";
+/// Channel
+const String channelName = "surfpay";
 
 /// Methods
-const String IS_READY_TO_PAY = "is_ready_to_pay";
-const String PAY = "pay";
-const String INIT = "init";
+const String isReadyToPay = "is_ready_to_pay";
+const String payMethod = "pay";
+const String initMethod = "init";
 
-const String ON_SUCCESS = "payment_success";
-const String ON_CANCEL = "payment_cancel";
-const String ON_ERROR = "payment_error";
+/// Payment result callbacks
+const String onSuccessCallback = "payment_success";
+const String onCancelCallback = "payment_cancel";
+const String onErrorCallback = "payment_error";
 
-const String ON_PAYMENT_TOKEN = "onPaymentToken";
-const String ON_PAYMENT_RESULT = "onPaymentResult";
+/// Return token for sending to server (IOS only)
+const String onPaymentResultCallback = "onPaymentResult";
 
 /// Arguments
-const String PAYMENT_ERROR_STATUS = "status";
-const String ON_SUCCESS_DATA = "successData";
+const String paymentErrorStatus = "status";
+const String onSuccessData = "successData";
+const String isTest = "IS_TEST";
 
-const String IS_TEST = "IS_TEST";
+const String paymentTokenData = "paymentTokenData";
+const String paymentTokenTransition = "paymentTokenTransition";
+const String paymentTokenNetwork = "paymentTokenNetwork";
 
-const String PAYMENT_TOKEN_DATA = "paymentTokenData";
-const String PAYMENT_TOKEN_TRANSITION = "paymentTokenTransition";
-const String PAYMENT_TOKEN_NETWORK = "paymentTokenNetwork";
-
-const String IS_PAYMENT_SUCCESS = "isPaymentSuccess";
-
+/// On success payment callback, payment data only on Android
 typedef SuccessCallback = Function(Map<String, dynamic> paymentData);
+
+/// On payment error callback
 typedef ErrorCallback = Function(PaymentErrorStatus);
 
-/// TODO replace bool with dynamic and send Errors?
+/// On payment token callback for IOS, with arguments: data, transition, paymentMethodType
 typedef ApplePayTokenCallback = Future<bool> Function(
-    String, String, PaymentMethodType);
+  String,
+  String,
+  PaymentMethodType,
+);
 
 /// Controller to work with native
 class PaymentController {
@@ -55,7 +59,6 @@ class PaymentController {
     this.onPaymentTokenCallback,
     this.isTestEnvironment = true,
   })  : assert(googlePayData != null),
-        //todo
         assert(applePayData != null) {
     _initCallbackListener();
     _init(
@@ -68,32 +71,35 @@ class PaymentController {
   /// return payment token to confirm payment with ApplePay
   final ApplePayTokenCallback onPaymentTokenCallback;
 
+  /// Google Pay immutable data
   final GooglePayData googlePayData;
+
+  /// Apple Pay immutable data
   final ApplePayData applePayData;
+
+  /// set test Environment on Android
   final bool isTestEnvironment;
+
+  /// payment success callback
   final SuccessCallback onSuccess;
+
+  /// payment cancel callback
   final VoidCallback onCancel;
+
+  /// payment error callback
   final ErrorCallback onError;
 
-  MethodChannel _channel = MethodChannel(CHANNEL_NAME);
-
-  void pay(
-      GooglePaymentRequest googleRequest, ApplePaymentRequest appleRequest) {
-    if (Platform.isAndroid) {
-      return _payGoogle(googleRequest);
-    }
-    return _payApple(appleRequest);
-  }
+  MethodChannel _channel = MethodChannel(channelName);
 
   void _init(
     GooglePayData googlePayData,
     ApplePayData applePayData,
     bool isTestEnvironment,
   ) {
-    _channel.invokeMethod(INIT, {
+    _channel.invokeMethod(initMethod, {
       ...googlePayData.map(),
       ...applePayData.map(),
-      IS_TEST: isTestEnvironment,
+      isTest: isTestEnvironment,
     });
   }
 
@@ -102,23 +108,23 @@ class PaymentController {
       // ignore: missing_return
       (call) async {
         switch (call.method) {
-          case ON_SUCCESS:
+          case onSuccessCallback:
             var paymentData = Map<String, dynamic>();
-            if (call.arguments[ON_SUCCESS_DATA] != null) {
+            if (call.arguments[onSuccessData] != null) {
               paymentData = jsonDecode(
-                call.arguments[ON_SUCCESS_DATA] as String,
+                call.arguments[onSuccessData] as String,
               );
             }
             onSuccess?.call(paymentData);
             break;
-          case ON_CANCEL:
+          case onCancelCallback:
             onCancel?.call();
             break;
-          case ON_ERROR:
-            final errorStatus = call.arguments[PAYMENT_ERROR_STATUS] as int;
+          case onErrorCallback:
+            final errorStatus = call.arguments[paymentErrorStatus] as int;
             onError?.call(getPaymentErrorStatus(errorStatus));
             break;
-          case ON_PAYMENT_RESULT:
+          case onPaymentResultCallback:
             return onPaymentToken(call);
             break;
         }
@@ -126,28 +132,38 @@ class PaymentController {
     );
   }
 
+  /// adaptive pay method
+  void pay(
+      GooglePaymentRequest googleRequest, ApplePaymentRequest appleRequest) {
+    if (Platform.isAndroid) {
+      return _payGoogle(googleRequest);
+    }
+    return _payApple(appleRequest);
+  }
+
   /// Checking if service enabled and paymentMethods enabled
   /// if true show button
   Future<bool> isServiceAvailable() {
-    return _channel.invokeMethod(IS_READY_TO_PAY);
+    return _channel.invokeMethod(isReadyToPay);
   }
 
+  /// callback for payment token, only IOS
   Future<bool> onPaymentToken(MethodCall call) {
     final params = call.arguments as Map<dynamic, dynamic>;
 
-    final data = params[PAYMENT_TOKEN_DATA] as String;
-    final transition = params[PAYMENT_TOKEN_TRANSITION] as String;
-    final methodType =
-        PaymentMethodType.byValue(params[PAYMENT_TOKEN_NETWORK] as int);
+    final data = params[paymentTokenData] as String;
+    final transition = params[paymentTokenTransition] as String;
+    final PaymentMethodType methodType =
+        PaymentMethodType.byValue(params[paymentTokenNetwork] as int);
     return onPaymentTokenCallback?.call(data, transition, methodType) ??
         Future.value(true);
   }
 
   void _payGoogle(GooglePaymentRequest request) {
-    _channel.invokeMethod(PAY, request.map());
+    _channel.invokeMethod(payMethod, request.map());
   }
 
   void _payApple(ApplePaymentRequest request) {
-    _channel.invokeMethod(PAY, request.map());
+    _channel.invokeMethod(payMethod, request.map());
   }
 }
