7ea10be0253bf08178d0bda5cf180bb11fec8c74
trushchinskii-surf <trushchinskii@surfstudio.ru>
2020-08-10 12:26:35 +0300

    FLT-359 - fix-pr

--- packages/surf_logger/lib/logger.dart
+++ packages/surf_logger/lib/logger.dart
@@ -14,9 +14,8 @@
 
 library logger;
 
-export 'package:logger/src/logger.dart' show Logger;
-export 'package:logger/src/strategies/impl/debug_strategy.dart';
-export 'package:logger/src/strategies/impl/remote_strategy.dart';
-
-export 'package:logger/src/remote/strategies/remote_log_user_strategy.dart';
-export 'package:logger/src/remote_logger.dart' show RemoteLogger;
+export 'package:surf_logger/src/logger.dart' show Logger;
+export 'package:surf_logger/src/remote/strategies/remote_log_user_strategy.dart';
+export 'package:surf_logger/src/remote_logger.dart' show RemoteLogger;
+export 'package:surf_logger/src/strategies/impl/debug_strategy.dart';
+export 'package:surf_logger/src/strategies/impl/remote_strategy.dart';
