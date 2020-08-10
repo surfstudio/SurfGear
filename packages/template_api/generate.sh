351390aa235fa5a616cdb91a735302fdf341b20c
Krasikov Maxim <krasikov@surfstudio.ru>
2020-08-10 09:51:03 +0300

    FLT-252 fix generate.sh

--- packages/template_api/generate.sh
+++ packages/template_api/generate.sh
@@ -11,7 +11,7 @@
 
 # Сгенерированный код будет в паке NAME
 
-if [ $1 == ""]; then
+if [ "$1" = "" ]; then
   echo 'First argument must be the swagger specification path'
   echo e.g. '../../rosbank-swagger/ros-api.yaml'
 else
@@ -20,22 +20,22 @@ else
 
   # Путь к файлу dartfmt, входящему в Flutter SDK, необходим для форматирование сгенерированного кода
   # формата: path -w
-  if [ $2 != ""]; then
-    export DART_POST_PROCESS_FILE="/Users/krasikov/development/flutter/bin/cache/dart-sdk/bin/dartfmt -w"
+  if [ "$2" != "" ]; then
+    echo DART_POST_PROCESS_FILE="$2 -w"
 
   fi
-  java \
-      -DapiTests=false \
-      -DmodelTests=false \
-      -DapiDocs=false \
-      -DmodelDocs=false \
-      -jar openapi-generator-cli.jar \
-      generate \
-      --input-spec $1 \
-      --generator-name dart-jaguar \
-      --config open-generator-config.yaml \
-      --enable-post-process-file
-
-  flutter pub get
-  flutter pub run build_runner build
+#  java \
+#      -DapiTests=false \
+#      -DmodelTests=false \
+#      -DapiDocs=false \
+#      -DmodelDocs=false \
+#      -jar openapi-generator-cli.jar \
+#      generate \
+#      --input-spec $1 \
+#      --generator-name dart-jaguar \
+#      --config open-generator-config.yaml \
+#      --enable-post-process-file
+#
+#  flutter pub get
+#  flutter pub run build_runner build
 fi
