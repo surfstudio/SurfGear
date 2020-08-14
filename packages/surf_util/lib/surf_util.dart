83feed1cf9bc484590aabffa3288888a0918143e
Krasikov Maxim <krasikov@surfstudio.ru>
2020-08-14 14:01:15 +0300

    FLT-388 delete unnassasary links

--- packages/surf_util/lib/surf_util.dart
+++ packages/surf_util/lib/surf_util.dart
@@ -19,7 +19,3 @@ export 'package:surf_util/src/enum/bitmask.dart';
 export 'package:surf_util/src/res/consts.dart';
 export 'package:surf_util/src/transformable.dart';
 export 'package:surf_util/src/ui/widget/disable_overscroll_widget.dart';
-
-export 'package:surf_util/src/ui/widget/refresh/cupertino_swipe_refresh.dart';
-export 'package:surf_util/src/ui/widget/refresh/material_swipe_refresh.dart';
-export 'package:surf_util/src/ui/widget/refresh/swipe_refresh.dart';
