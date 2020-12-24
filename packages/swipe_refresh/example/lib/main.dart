8328b7bbee656b099038f00e054e25b2fd8cc8ff
Aleksey Radionov <radionov@surfstudio.ru>
2020-12-24 14:39:46 +0300

    add screenshot to swipe_refresh package

--- packages/swipe_refresh/example/lib/main.dart
+++ packages/swipe_refresh/example/lib/main.dart
@@ -21,6 +21,7 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
