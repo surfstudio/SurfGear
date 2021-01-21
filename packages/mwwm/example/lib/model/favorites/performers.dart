0e5e96055b427986540b6c83f7bb997a38e74bf2
osipov-e <osipov-e@surfstudio.ru>
2021-01-21 15:33:47 +0300

    убрал лишнее

--- packages/mwwm/example/lib/model/favorites/performers.dart
+++ packages/mwwm/example/lib/model/favorites/performers.dart
@@ -19,60 +19,57 @@ import 'package:mwwm_github_client/model/favorites/repository/favorites_reposito
 import 'package:mwwm_github_client/ui/widgets/repository/repository_widget_wm.dart';
 
 class ToggleRepositoryFavoriteValuePerformer
-		extends FuturePerformer<void, ToggleRepositoryFavoriteValue> {
-		ToggleRepositoryFavoriteValuePerformer(this._favoritesRepository);
+    extends FuturePerformer<void, ToggleRepositoryFavoriteValue> {
+  ToggleRepositoryFavoriteValuePerformer(this._favoritesRepository);
 
-		final FavoritesRepository _favoritesRepository;
+  final FavoritesRepository _favoritesRepository;
 
-		@override
-		Future<void> perform(ToggleRepositoryFavoriteValue change) async {
-				if (change.isFavorite) {
-						await _favoritesRepository.add(change.repo);
-				} else {
-						await _favoritesRepository.delete(change.repo);
-				}
+  @override
+  Future<void> perform(ToggleRepositoryFavoriteValue change) async {
+    if (change.isFavorite) {
+      await _favoritesRepository.add(change.repo);
+    } else {
+      await _favoritesRepository.delete(change.repo);
+    }
 
-				favoritesChangedState.accept(true);
-		}
+    favoritesChangedState.accept(true);
+  }
 }
 
 class GetFavoriteRepositoriesPerformer
-		extends Broadcast<List<Repository>, GetFavoriteRepositories> {
-		GetFavoriteRepositoriesPerformer(this._favoritesRepository);
+    extends Broadcast<List<Repository>, GetFavoriteRepositories> {
+  GetFavoriteRepositoriesPerformer(this._favoritesRepository);
 
-		final FavoritesRepository _favoritesRepository;
+  final FavoritesRepository _favoritesRepository;
 
-		@override
-		Future<List<Repository>> performInternal(GetFavoriteRepositories change) {
-				return _favoritesRepository.getRepositories();
-		}
+  @override
+  Future<List<Repository>> performInternal(GetFavoriteRepositories change) {
+    return _favoritesRepository.getRepositories();
+  }
 }
 
 class DefineFavoritesFromRepositoryPerformer
-		extends FuturePerformer<List<Repository>, DefineFavoritesFromRepository> {
-		DefineFavoritesFromRepositoryPerformer(this._favoritesRepository);
+    extends FuturePerformer<List<Repository>, DefineFavoritesFromRepository> {
+  DefineFavoritesFromRepositoryPerformer(this._favoritesRepository);
 
-		final FavoritesRepository _favoritesRepository;
+  final FavoritesRepository _favoritesRepository;
 
-		@override
-		Future<List<Repository>> perform(
-				DefineFavoritesFromRepository change) async {
-				final List<Repository> repositories =
-						List?.from(change?.repositories ?? []) ?? [];
-				final List<Repository> favorites =
-				await _favoritesRepository.getRepositories();
+  @override
+  Future<List<Repository>> perform(DefineFavoritesFromRepository change) async {
+    final List<Repository> repositories = List.from(change?.repositories ?? []);
+    final List<Repository> favorites =
+        await _favoritesRepository.getRepositories();
 
-				for (Repository repo in repositories) {
-						repo.isFavorite = false;
-				}
+    for (Repository repo in repositories) {
+      repo.isFavorite = false;
+    }
 
-				for (Repository fav in favorites) {
-						final Repository repo = repositories.firstWhere(
-										(repo) => repo.id == fav.id, orElse: () => null
-						);
-						repo?.isFavorite = true;
-				}
+    for (Repository fav in favorites) {
+      final Repository repo = repositories
+          .firstWhere((repo) => repo.id == fav.id, orElse: () => null);
+      repo?.isFavorite = true;
+    }
 
-				return repositories;
-		}
+    return repositories;
+  }
 }
