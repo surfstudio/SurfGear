c09dc4e13baf56dfc5759b718fa976c2dab49a96
Aleksandr Trushchinskii <51919090+trushchinskii-surf@users.noreply.github.com>
2021-04-02 14:52:17 +0300

    FLT-1235 [VS] Генерация виджетов на основе CoreMwwmWidget (#111)
    * FLT-1235 - добавлены файлы  .vscode
    
    * FLT-1235 - Added debug folder to launch.json
    
    * FLT-1235 - Added mwwm-widget creating
    
    * FLT-1235 - Увеличена версия плагина до 0.0.5
    
    * FLT-1235 - Правки пр
    
    * FLT-1235 - Откатил package.json чтобы можно было запаковать его

---
+++ tools/ide/vs_code/extensions/mwwm_generator/templates/mwwm_widget/template_wm.dart
@@ -0,0 +1,14 @@
+import 'package:surf_mwwm/surf_mwwm.dart';
+
+class TemplateWm extends WidgetModel {
+  TemplateWm(
+    WidgetModelDependencies dependencies,
+  ) : super(dependencies);
+
+  @override
+  void onLoad() {
+    super.onLoad();
+
+    // TODO
+  }
+}
