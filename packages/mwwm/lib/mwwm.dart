ddef56ee09308349837f8d68ea9fe62e2e7b6ed8
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-04-14 10:43:48 +0300

    fix: fix some typos in mwwm readme

--- packages/mwwm/lib/mwwm.dart
+++ packages/mwwm/lib/mwwm.dart
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-library mwwm;
-
 //errors
 export 'package:mwwm/src/error/error_handler.dart';
 
