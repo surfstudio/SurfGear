6f0258da2e030b302112d71940f9b56b2973fd40
Danila Gogolev <dagogolev@gmail.com>
2021-04-15 10:25:19 +0300

    Remove tests

--- packages/mwwm/test/mocks/performer_mock.dart
+++
@@ -1,6 +0,0 @@
-import 'package:mocktail/mocktail.dart';
-import 'package:mwwm/src/model/performer/performer.dart';
-import 'package:mwwm/src/model/changes/changes.dart';
-
-class PerformerMock<R, C extends Change<R>> extends Mock
-    implements Performer<R, C> {}
--- packages/mwwm/test/mocks/steam_subscription_mock.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:async';
-
-import 'package:mocktail/mocktail.dart';
-
-class StreamSubscriptionMock<T> extends Mock implements StreamSubscription<T> {}
