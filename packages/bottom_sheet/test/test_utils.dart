23b328136e3c6727d7b6532324478294bf331d52
Denis Grafov <grafov.denis@gmail.com>
2021-04-20 16:13:31 +0300

    add tests

---
+++ packages/bottom_sheet/test/test_utils.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Widget makeTestableWidget(Widget widget) => MaterialApp(
+      home: Scaffold(
+        body: SizedBox.expand(child: widget)
+      ),
+    );
