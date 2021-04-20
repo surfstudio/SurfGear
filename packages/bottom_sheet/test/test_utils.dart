eaef04aeba965b216bf769b8b017d3b606080cb6
Denis Grafov <grafov.denis@gmail.com>
2021-04-20 16:16:06 +0300

    format

--- packages/bottom_sheet/test/test_utils.dart
+++ packages/bottom_sheet/test/test_utils.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
 Widget makeTestableWidget(Widget widget) => MaterialApp(
-      home: Scaffold(
-        body: SizedBox.expand(child: widget)
-      ),
+      home: Scaffold(body: SizedBox.expand(child: widget)),
     );
