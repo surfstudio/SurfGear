99afa6e8bcb7bc7fc07a2da9026776ed04046c90
Denis Grafov <grafov.denis@gmail.com>
2021-04-21 11:26:52 +0300

    add test

--- packages/otp_autofill/test/otp_autofill_test.dart
+++ packages/otp_autofill/test/otp_autofill_test.dart
@@ -1,22 +1,42 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:otp_autofill/base/strategy.dart';
+import 'package:otp_autofill/otp_text_edit_controller.dart';
+
+const testCode = '54321';
+
+class SampleStrategy extends OTPStrategy {
+  @override
+  Future<String> listenForCode() {
+    return Future.delayed(
+      const Duration(milliseconds: 250),
+      () => 'Your code is $testCode',
+    );
+  }
+}
 
 void main() {
-  const MethodChannel channel = MethodChannel('otp_autofill');
+  // This code binds the [TestWidgetsFlutterBinding]
+  // to a [LiveTestWidgetsFlutterBinding],
+  // which runs test code in a real async zone.
+  LiveTestWidgetsFlutterBinding();
 
-  TestWidgetsFlutterBinding.ensureInitialized();
+  late OTPTextEditController controller;
 
   setUp(() {
-    channel.setMockMethodCallHandler((methodCall) async {
-      return '42';
-    });
+    controller = OTPTextEditController(
+      codeLength: testCode.length,
+      onCodeReceive: (code) {},
+    )..startListenOnlyStrategies([SampleStrategy()], (code) {
+        final exp = RegExp(r'(\d{5})');
+        return exp.stringMatch(code ?? '') ?? '';
+      });
   });
 
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
+  testWidgets('OTPTextEditController startListenOnlyStrategies', (tester) async {
+    expect(controller.text, isEmpty);
 
-  test('getTelephoneHint', () {
-    expect(true, true);
+    await tester.pump(const Duration(milliseconds: 500));
+
+    expect(controller.text, equals(testCode));
   });
 }
