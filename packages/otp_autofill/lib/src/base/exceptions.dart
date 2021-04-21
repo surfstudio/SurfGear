a05136c95c696c32aca2b7a0bcdcfc8dbea8407c
Denis Grafov <grafov.denis@gmail.com>
2021-04-21 15:07:30 +0300

    format

--- packages/otp_autofill/lib/src/base/exceptions.dart
+++ packages/otp_autofill/lib/src/base/exceptions.dart
@@ -11,6 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
- 
+
 /// Plugin doesn't support platform
 class UnsupportedPlatform implements Exception {}
