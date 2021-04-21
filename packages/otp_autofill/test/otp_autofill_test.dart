b109fe5fc1fc7fb9c9309c31cdb666362a200d22
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-04-21 14:54:33 +0300

    add missed licanses

--- packages/otp_autofill/test/otp_autofill_test.dart
+++ packages/otp_autofill/test/otp_autofill_test.dart
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:otp_autofill/src/base/strategy.dart';
 import 'package:otp_autofill/src/otp_text_edit_controller.dart';
