d3f1999aaa42be588ec4df61dea0155ef3cbf013
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-04-26 15:15:50 +0300

    get rid flutter from project

--- packages/datalist/test/datalist_offset_test.dart
+++ packages/datalist/test/datalist_offset_test.dart
@@ -14,7 +14,7 @@
 
 import 'package:datalist/src/impl/datalist_limit_offset.dart';
 import 'package:datalist/src/exceptions.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 void main() {
   test('testCheckNormalMerge', () {
--- packages/datalist/test/datalist_pagecount_test.dart
+++ packages/datalist/test/datalist_pagecount_test.dart
@@ -14,7 +14,7 @@
 
 import 'package:datalist/src/impl/datalist_page_count.dart';
 import 'package:datalist/src/exceptions.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 void main() {
   test('testNormalMerge', () {
