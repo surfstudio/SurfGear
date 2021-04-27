fce68f9006d98f5e4f2df876787c0bc3898509f7
Denis Grafov <grafov.denis@gmail.com>
2021-04-27 15:06:03 +0300

    add navigation between tabs test

--- packages/tabnavigator/test/tab_navigator_test.dart
+++ packages/tabnavigator/test/tab_navigator_test.dart
@@ -18,7 +18,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tabnavigator/tabnavigator.dart';
 
 void main() {
-  final _tabController = StreamController<TestTab>.broadcast();
+  final _tabController = StreamController<TestTab>.broadcast(sync: true);
 
   group('TabNavigator', () {
     testWidgets('smoke test', (tester) async {
@@ -43,8 +43,82 @@ void main() {
       );
 
       await tester.pumpWidget(_widget);
+    });
 
-      expect(true, isTrue);
+    testWidgets('navigation between tabs', (tester) async {
+      const _initTab = TestTab.first;
+
+      const _keys = [
+        Key('first'),
+        Key('second'),
+        Key('third'),
+      ];
+
+      final _map = <TestTab, TabBuilder>{
+        TestTab.first: () => Container(color: Colors.white, key: _keys[0]),
+        TestTab.second: () => Container(color: Colors.blue, key: _keys[1]),
+        TestTab.third: () => Container(color: Colors.red, key: _keys[2]),
+      };
+
+      Stream<TestTab> tabStream() => _tabController.stream;
+
+      final _widget = MaterialApp(
+        home: Scaffold(
+          body: TabNavigator(
+            initialTab: _initTab,
+            selectedTabStream: tabStream(),
+            mappedTabs: _map,
+          ),
+        ),
+      );
+
+      await tester.pumpWidget(_widget);
+
+      // current tab is first with white color
+      expect(
+        find.descendant(
+          of: find.byType(TabNavigator),
+          matching: find.byKey(_keys[0]),
+        ),
+        findsWidgets,
+      );
+
+      expect(
+        find.descendant(
+          of: find.byType(TabNavigator),
+          matching: find.byWidgetPredicate(
+            (widget) =>
+                widget is Container &&
+                (widget.color == Colors.blue || widget.color == Colors.red),
+          ),
+        ),
+        findsNothing,
+      );
+
+      // set current tab to second with blue color
+      _tabController.sink.add(TestTab.second);
+      await tester.pump();
+
+      // current tab is second with blue color
+      expect(
+        find.descendant(
+          of: find.byType(TabNavigator),
+          matching: find.byKey(_keys[1]),
+        ),
+        findsWidgets,
+      );
+
+      expect(
+        find.descendant(
+          of: find.byType(TabNavigator),
+          matching: find.byWidgetPredicate(
+            (widget) =>
+                widget is Container &&
+                (widget.color == Colors.red || widget.color == Colors.white),
+          ),
+        ),
+        findsNothing,
+      );
     });
   });
 
