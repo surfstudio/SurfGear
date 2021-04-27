059ba52e51496f52880f092bcc7374db3b05ba3a
Denis Grafov <grafov.denis@gmail.com>
2021-04-27 14:16:39 +0300

    rm surf_utils dependency

--- packages/tabnavigator/lib/src/tab_type.dart
+++ packages/tabnavigator/lib/src/tab_type.dart
@@ -12,23 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'package:surf_util/surf_util.dart';
-
 /// base type for a list of tabs
-/// adding new types of tabs via the create() method
-abstract class TabType extends Enum<int> {
-  const TabType(int value) : super(value);
+/// adding new types of tabs via the [append] method
+abstract class TabType {
+  const TabType(this.value);
 
-  static const emptyValue = -1;
+  final int value;
 
-  static const values = <TabType>[];
+  static const int emptyValue = -1;
 
-  static void create(TabType newTab) {
-    values.add(newTab);
+  static const _values = <TabType>[];
+
+  Iterable<TabType> get values => _values;
+
+  static void append(TabType newTab) {
+    _values.add(newTab);
   }
 
   static TabType byValue(int ordinal) {
-    return values.firstWhere(
+    return _values.firstWhere(
       (value) => ordinal == value.value,
       orElse: () => throw Exception('Unknown TabType by ordinal $ordinal'),
     );
