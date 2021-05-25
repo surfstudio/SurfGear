0f88bfe88cf0e40d93cdebbde1daf4f334dac38b
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-05-25 06:31:57 +0300

    fix

--- packages/datalist/lib/datalist.dart
+++ packages/datalist/lib/datalist.dart
@@ -16,4 +16,3 @@ export 'package:datalist/src/datalist.dart';
 export 'package:datalist/src/exceptions.dart';
 export 'package:datalist/src/impl/datalist_limit_offset.dart';
 export 'package:datalist/src/impl/datalist_page_count.dart';
-
