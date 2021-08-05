d0d952d01daaacfc9f1bab45f55644a1044ad2ae
Konstantin Kuzminov <zal1358@gmail.com>
2021-08-05 17:35:21 +0200

    unit test on weather API calls

--- examples/weather/test/modules/weather/repository/weather_repository_test.dart
+++ examples/weather/test/modules/weather/repository/weather_repository_test.dart
@@ -19,6 +19,13 @@ void main() {
     'q': 'Moscow',
   };
 
+  final Map<String, String> paramsGeo = {
+    'lat': 10.0.toString(),
+    'lon': 10.0.toString(),
+    'units': 'metric',
+    'appid': 'f6c94efdd8a88e35fd00a12d8beab998',
+  };
+
   const String weatherResp =
       '{"coord":{"lon":-0.1257,"lat":51.5085},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03d"}],"base":"stations","main":{"temp":18.84,"feels_like":18.76,"temp_min":16.61,"temp_max":21.81,"pressure":1003,"humidity":76},"visibility":10000,"wind":{"speed":6.17,"deg":160},"clouds":{"all":40},"dt":1628175620,"sys":{"type":2,"id":268730,"country":"GB","sunrise":1628137828,"sunset":1628192550},"timezone":3600,"id":2643743,"name":"London","cod":200}';
 
@@ -47,6 +54,29 @@ void main() {
 
       verify(() =>
               weatherApiClientMock.get('/data/2.5/weather', params: params))
+          .called(1);
+    },
+  );
+
+  test(
+    'getWeatherGeoLocation should make correct API request',
+    () {
+      when(
+        () => weatherApiClientMock.get(
+          '/data/2.5/weather',
+          params: paramsGeo,
+        ),
+      ).thenAnswer(
+        (invocation) => Future.value(
+          //TODO: уточнить, как правильно. Невалидный JSON кидает ошибки.
+          Response(weatherResp, 200),
+        ),
+      );
+
+      weatherRepository.getWeatherGeolocation(10.0, 10.0);
+
+      verify(() =>
+              weatherApiClientMock.get('/data/2.5/weather', params: paramsGeo))
           .called(1);
     },
   );
