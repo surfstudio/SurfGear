dd74144ec5b3f5b3c09d86f79af4c0966fea4cfc
Deeyw Vladimir <deyew@surfstudio.ru>
2021-09-15 11:38:29 +0700

    project created

---
+++ packages/mwwm/example2/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        primarySwatch: Colors.blue,
+      ),
+      home: 
+    );
+  }
+}
+
